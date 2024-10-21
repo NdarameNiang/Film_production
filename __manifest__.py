549dda7a882f8d82b4c372141ffd9a3258569edb
unknown <arame2001.an@gmail.com>
2024-10-21 20:19:45 +0000

    Initial commit for Film production module

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Film Production',
+    'version': '1.0',
+    'category': 'Entertainment',
+    'summary': 'Module for managing film productions',
+    'description': 'Module for managing films, locations, production companies, shootings, and directors.',
+    'depends': ['base'],
+    'data': [
+        'views/film_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
